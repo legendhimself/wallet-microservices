7c0d124628355ccd9df93fe2c6507a6a5b0cb641
legendhimslef <sabdulrahman971@gmail.com>
2022-10-15 12:12:12 +0530

    fix: seeding

--- src/mongoose/seeder/seeder.service.ts
+++ src/mongoose/seeder/seeder.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { stat } from 'fs/promises';
 import { Connection } from 'mongoose';
+import { Readable } from 'stream';
 import { promisify } from 'util';
+
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { createWriteStream } from 'fs';
 
 import { ConfigService } from '@nestjs/config';
 import { exec } from 'child_process';
@@ -21,40 +25,59 @@ export class SeederService {
     if (count < 10) {
       console.log('Seeding...', count);
       await this.getData();
-      // users --drop --jsonArray --batchSize 1 --file ./users.json
+
       const { stderr, stdout } = await runCommand(
         `mongoimport  ${this.config.get(
           'DB_URI',
         )} --collection users --drop --jsonArray --batchSize 100 --file db-snapshot.json`,
       );
-      console.log('Seeding Done');
-      console.log(stderr, stdout);
-    }
+      console.log('Seeding done.');
+      if (stderr) console.error(stderr);
+      else console.log(stdout);
+    } else console.log('Seeding already done.');
   }
 
   async getData() {
     return new Promise<boolean>(async (resolve, reject) => {
       try {
-        const fileStat = await stat('db-snapshot.json');
+        const fileStat = await stat('db-snapshot.json').catch(() => ({
+          size: 0,
+        }));
         // fetch from bucket once
-        if (fileStat.size > 0) return resolve(true);
+        if (fileStat.size > 10) return resolve(true);
+        console.log('Fetching data from bucket.');
+        const bucket = new S3Client({
+          region: 'eu-central-1',
+          signer: { sign: async (request) => request },
+        });
+        const createSt = createWriteStream('./db-snapshot.txt');
+        const finalSt = createWriteStream('./db-snapshot.json');
 
-        // const bucket = new S3Client({
-        //   region: 'eu-central-1',
-        //   signer: { sign: async (request) => request },
-        // });
+        const st = await bucket.send(
+          new GetObjectCommand({
+            Bucket: 'nt-interview-files',
+            Key: 'data.json',
+          }),
+        );
+        const readable = st.Body as Readable;
 
-        // const createSt = createWriteStream('./db-snapshot.json');
-        // const response = await bucket.send(
-        //   new GetObjectCommand({
-        //     Bucket: 'nt-interview-files',
-        //     Key: 'data.json',
-        //   }),
-        // );
-        // if (response.Body) {
-        //   createSt.on('finish', () => resolve(true));
-        //   (response.Body as Stream).pipe(createSt);
-        // }
+        let lastEle = '';
+        finalSt.write('[');
+        readable.on('data', (d) => {
+          if (lastEle) finalSt.write(lastEle);
+          const chunk = d.toString().split('\n').join(',\n');
+          lastEle = chunk;
+        });
+
+        readable.on('end', async () => {
+          finalSt.write(lastEle.trim().replace(/,$/, ''));
+          finalSt.write('\n]');
+          finalSt.end();
+          console.log('Fetching done.');
+          resolve(true);
+        });
+
+        readable.pipe(createSt);
       } catch (error) {
         reject(error);
       }
